d70a1b8bbfa697477271370f2ff98c24ae761485
zhakobaltabai <zhandos.baltabai@gmail.com>
2022-12-20 05:05:21 +0600

    ZH

---
+++ final/css/index5.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style3.css">
+    <link rel="stylesheet" href="css/mobile.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Page 2</title>
+</head>
+
+<body>
+<header>
+        <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRchksNSh4fZ_QZl78rgH2tORDJkpDlxYJ9p1ggxPBZNw&s" id='iconToyota'>
+         <a id='textToyota' href="http://localhost/GroupProjectToyota/index.php">   Toyota</a>
+        </div>
+    
+        <!-- <div id='siteButton'> -->
+            <div id="divSpaseAround"><a id="siteLink" href="http://localhost/GroupProjectToyota/index5.php"> Vehicles</a></div>
+            <div id="divSpaseAround"> <a id="siteLink" href="http://localhost/GroupProjectToyota/index3.php">TradeIn</a></div>
+            <div id="divSpaseAround"> <a id="siteLink" href="http://localhost/GroupProjectToyota/index2.php">Electro</a></div>
+            <div id="divSpaseAround"> <a id="siteLink" href="http://localhost/GroupProjectToyota/index4.php"><img src="https://www.pngkit.com/png/full/808-8088160_loginsignupwishlist-transparent-login-icon.png" id='iconLogin'></a></div>
+        <!-- </div>/ -->
+
+    </header>
+
+
+
+
+    <div id='flexConteiner'>
+
+        <div id='divInfoMeatt'>
+            <div style="width:100%">
+                <img id='imgMeat' src='https://files.toyota.com.bh/s3fs-public/2019-11/White-Pearl-Crystal-Shine_7.png'>
+            </div>
+            <div style="width:50%">
+                <h1 id='titleMeat'>Toyota Carolla</h1>
+                <p style="color:white;margin:5%">
+
+                    Цена : от 11 500 000 тг <br>
+                    Количество мест : 5 <br>
+                    Максимальная скорость (км/ч) : 240 km/h <br>
+                    Мощность (л.с.) : 220<br>
+                </p>
+
+            </div>
+
+        </div>
+
+
+
+        <div id='divInfoMeatt'>
+            <div style="width:100%">
+                <img id='imgMeat' src='https://file.kelleybluebookimages.com/kbb/base/evox/CP/14668/2022-Toyota-C-HR-front_14668_032_1831x844_1G3_cropped.png'>
+            </div>
+            <div style="width:50%">
+                <h1 id='titleMeat'>Toyota C-HR</h1>
+                <p style="color:white;margin:5%">
+
+                    Цена : от 15 500 000 тг <br>
+                    Количество мест : 5 <br>
+                    Максимальная скорость (км/ч) : 240 km/h <br>
+                    Мощность (л.с.) : 240<br>
+                </p>
+            </div>
+
+
+        </div>
+    </div>
+
+
+
+
+
+    <div id='flexConteiner'>
+
+        <div id='divInfoMeatt'>
+            <div style="width:100%">
+                <img id='imgMeat' src='http://images.dealer.com/ddc/vehicles/2022/Toyota/RAV4/SUV/trim_LE_da7ab0/perspective/front-left/2022_76.png'>
+            </div>
+            <div style="width:50%">
+                <h1 id='titleMeat'>RAV 4</h1>
+                <p style="color:white;margin:5%">
+
+                    Цена : от 21 500 000 тг <br>
+                    Количество мест : 5 <br>
+                    Максимальная скорость (км/ч) : 240 km/h <br>
+                    Мощность (л.с.) : 220<br>
+                </p>
+            </div>
+
+        </div>
+
+
+
+        <div id='divInfoMeatt'>
+            <div style="width:100%">
+                <img id='imgMeat' src='https://www.toyota.co.nz/globalassets/car-images/highlander-hkax-si2-3t3-10.png'>
+            </div>
+            <div style="width:50%">
+                <h1 id='titleMeat'>Highlander</h1>
+                <p style="color:white;margin:5%">
+
+                    Цена : от 33 500 000 тг <br>
+                    Количество мест : 5 <br>
+                    Максимальная скорость (км/ч) : 270 km/h <br>
+                    Мощность (л.с.) : 420<br>
+                </p>
+            </div>
+
+
+        </div>
+    </div>
+
+    <div id='flexConteiner'>
+
+        <div id='divInfoMeatt'>
+            <div style="width:100%;">
+                <img id='imgMeat' src='https://toyota-tegeta.ge/storage/4374/conversions/day-exterior-33_1f7-webp.webp'>
+            </div>
+            <div style="width:50%">
+                <h1 id='titleMeat'>Land Cruiser Prado</h1>
+                <p style="color:white;margin:5%">
+
+                    Цена : от 31 500 000 тг <br>
+                    Количество мест : 5 <br>
+                    Максимальная скорость (км/ч) : 240 km/h <br>
+                    Мощность (л.с.) : 320<br>
+                </p>
+            </div>
+
+        </div>
+
+
+
+        <div id='divInfoMeatt'>
+            <div style="width:100%">
+                <img id='imgMeat' src='https://www.pngmart.com/files/22/Toyota-Hilux-PNG-Image.png'>
+            </div>
+            <div style="width:50%">
+                <h1 id='titleMeat'>Hilux</h1>
+                <p style="color:white;margin:5%">
+
+                    Цена : от 21 500 000 тг <br>
+                    Количество мест : 5 <br>
+                    Максимальная скорость (км/ч) : 240 km/h <br>
+                    Мощность (л.с.) : 220<br>
+                </p>
+            </div>
+
+
+        </div>
+    </div>
+    <div id='flexConteiner'>
+
+        <div id='divInfoMeatt'>
+            <div style="width:100%">
+                <img id='imgMeat' src='https://www.toyotaqatar.com/myassets/uploads/images/Vehicles/Land%20Cruiser/Colors/090%20Precious%20White%20Pearl_1.png'>
+            </div>
+            <div style="width:50%">
+                <h1 id='titleMeat'>LC 300</h1>
+                <p style="color:white;margin:5%">
+
+                    Цена : от 51 500 000 тг <br>
+                    Количество мест : 5 <br>
+                    Максимальная скорость (км/ч) : 240 km/h <br>
+                    Мощность (л.с.) : 220<br>
+                </p>
+            </div>
+
+        </div>
+
+
+
+        <div id='divInfoMeatt'>
+            <div style="width:100%">
+                <img id='imgMeat' src='https://www.pngplay.com/wp-content/uploads/13/Toyota-Camry-Transparent-Images.png'>
+            </div>
+            <div style="width:50%">
+                <h1 id='titleMeat'>Camry</h1>
+                <p style="color:white;margin:5%">
+
+                    Цена : от 21 500 000 тг <br>
+                    Количество мест : 5 <br>
+                    Максимальная скорость (км/ч) : 240 km/h <br>
+                    Мощность (л.с.) : 220<br>
+                </p>
+            </div>
+
+
+        </div>
+    </div>
+    <footer>
+
+
+
+    </footer>
+</body>
+
+</html>
